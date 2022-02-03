73d09b1fcd83d771cb4f0e48c26000d8579a6280
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2022-02-04 01:24:38 +0900

    Changed model

--- Habitually/Habit.swift
+++ Habitually/Habit.swift
@@ -9,9 +9,13 @@ import Foundation
 
 struct Habit: Codable, Identifiable {
     let habitType: HabitType
-    var timePerformed: TimeInterval
     var description: String?
+    var timePerformed: TimeInterval
     var title: String
     var daysCompleted: Int
     var id = UUID()
+    
+    var investedTimeInHours: Double {
+        timePerformed * Double(daysCompleted) / 60.0
+    }
 }
