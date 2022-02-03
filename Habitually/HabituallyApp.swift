21bfd3f75a3bc15716201803a4a27fd7bb80c333
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2022-02-03 17:22:48 +0900

    Initial Commit

---
+++ Habitually/HabituallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabituallyApp.swift
+//  Habitually
+//
+//  Created by Sankarshana V on 2022/02/03.
+//
+
+import SwiftUI
+
+@main
+struct HabituallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
